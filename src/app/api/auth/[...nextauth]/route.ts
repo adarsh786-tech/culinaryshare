9e2bea08213d0fa6356452f24239c11d6286a1a9
mihir <mihir.1si17cs058@gmail.com>
2024-09-09 00:52:03 +0530

    Added fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,64 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import bcrypt from "bcrypt";
+import NextAuth, { NextAuthOptions } from "next-auth";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from "@/lib/prismadb";
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_AUTH_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_AUTH_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_AUTH_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_AUTH_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error(`Invalid Credentials`);
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        });
+        if (!user || !user?.hashedPassword) {
+          throw new Error(`Invalid Credentials`);
+        }
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error(`Invalid Credentials`);
+        }
+        return user;
+      },
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    // Use JSON Web Tokens for session instead of database sessions.
+    strategy: "jwt",
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 const handler = NextAuth(authOptions);
 
