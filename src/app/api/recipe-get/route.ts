94c312bbc2ac5d93eb63727254fc69b0c4504284
mihir <mihir.1si17cs058@gmail.com>
2024-09-09 01:49:51 +0530

    minor fix

--- src/app/api/recipe-get/route.ts
+++ src/app/api/recipe-get/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import prismadb from "@/lib/prismadb";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const recipeList = await prismadb.recipe.findMany();
